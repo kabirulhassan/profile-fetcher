89f85d2f132d20ba809b9e989b78668b134ec985
kabirulhassan <kabirulhassan7@gmail.com>
2022-11-21 00:10:35 +0530

    added basic pagination component

--- src/components/PaginationComponent.js
+++ src/components/PaginationComponent.js
@@ -2,17 +2,29 @@ import React from "react";
 
 const PaginationComponent = (props) => {
     const { page, setPage, totalPages } = props;
+    const pageNumbers = [];
+    for(let i = 1; i <= totalPages; i++){
+        pageNumbers.push(i);
+    }
     return (
         <div className="pagination">
+        <nav>
+            <ul>
             <button
                 onClick={() => setPage(page - 1)}
                 disabled={page === 1}
             >Prev</button>
-            <span>{page}</span>
+            {pageNumbers.map(number => (
+                <li key={number}>
+                    <a onClick={() => setPage(number)}>{number}</a>
+                </li>
+            ))}
             <button
                 onClick={() => setPage(page + 1)}
                 disabled={page === totalPages}
             >Next</button>
+            </ul>
+        </nav>
         </div>
     );
 }
